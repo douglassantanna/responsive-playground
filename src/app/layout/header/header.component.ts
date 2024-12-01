6aba0c22cc2dc099c7fd7a45af1d841495632123
Douglas SantAnna Figueredo <68830911+douglassantanna@users.noreply.github.com>
2024-12-01 22:32:58 +0000

    remove welcome message and add user inital name letter

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,13 +1,31 @@
+import { SlicePipe } from '@angular/common';
 import { Component, output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [SlicePipe],
   templateUrl: './header.component.html',
   styles: ``
 })
 export class HeaderComponent {
   toggleSidenavEvent = output();
-  userName: string = 'John';
+  userName: string = 'John Johnson';
+  dropdownOpen = false;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  profile() {
+    console.log('Profile clicked');
+  }
+
+  settings() {
+    console.log('Settings clicked');
+  }
+
+  logout() {
+    console.log('Logout clicked');
+  }
 }
